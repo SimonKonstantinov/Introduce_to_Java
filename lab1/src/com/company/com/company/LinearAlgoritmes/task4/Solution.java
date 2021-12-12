879775ea7c2c8544a3cb29690353b5d67dcbce62
SimonKonstantinov <33290779+SimonKonstantinov@users.noreply.github.com>
2021-12-12 21:56:16 +0300

    add linearAlg task4 and branching task1

---
+++ lab1/src/com/company/com/company/LinearAlgoritmes/task4/Solution.java
@@ -0,0 +1,32 @@
+package com.company.LinearAlgoritmes.task4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // write your code here
+        double x;
+
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            x = Double.parseDouble(reader.readLine());
+            System.out.println(equation(x));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static String equation(double x) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        String value = df.format(x);
+        String[] words = value.split(",");
+        return words[1] + "." + words[0];
+    }
---
+++ lab1/src/com/company/com/company/branching/task1/Solution.java
@@ -0,0 +1,34 @@
+package com.company.branching.task1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a;
+        int b;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            a = Integer.parseInt((reader.readLine()));
+            b = Integer.parseInt((reader.readLine()));
+
+            System.out.println("треугольник сущесвует "+existsTriangle(a,b));
+            System.out.println("треугольник прямоугольный "+rightTriangle(a,b));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static boolean existsTriangle(int a, int b) {
+        return (a+b)<180 && a>0 && b>0;
+    }
+    static boolean rightTriangle(int a, int b) {
+        return (a+b)==90;
+    }
+
+}
